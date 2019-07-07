b8f98afc49f7a05c2ba6ae428346b179990a230f
ScottShelton <shelton.scott25f@outlook.com>
2019-07-07 12:33:40 -0600

    Initial project build and commit.

---
+++ Get Playin! 2.0/Get Playin! 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get Playin! 2.0
+//
+//  Created by scott shelton on 7/7/19.
+//  Copyright © 2019 scott shelton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
